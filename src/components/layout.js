da4c584058f296352a3b7057af5c852fd887a5c5
Gabrielle <eastongabrielle@gmail.com>
2020-10-09 22:23:20 -0400

    add styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 //import { useStaticQuery, graphql } from "gatsby"
-import Header from "./Header"
+import Header from "./header"
 import Footer from "./Footer"
 import Sidebar from "./Sidebar"
 import "./layout.css"
