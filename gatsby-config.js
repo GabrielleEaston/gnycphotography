cc704ba017dad90157b5df0cae5aae6a7cb4777e
Gabrielle <eastongabrielle@gmail.com>
2020-10-09 22:19:32 -0400

    add styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gonzalo Cisterna | Photograpy`,
+    title: `Gonzalo Cisterna | Photography`,
     description: `NYC based | Photography`,
     author: `@gabrielle`,
   },
